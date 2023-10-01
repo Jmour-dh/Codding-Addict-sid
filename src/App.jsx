033d936fb6b812cfc837e8bc6300aad8fea9a366
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-10-01 23:23:12 +0200

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+};
 
-export default App
+export default App;
